8ca9c2a1e9103e1f0eeb8cf9be90124e35eeac27
Liujiayi <liujy20010831@icloud.com>
2020-04-04 22:50:22 +0800

    fea:2020.4.4
    - 网格化
    - a*算法
    - 平滑处理

---
+++ 基于网格化的a*/Assets/Scripts/Map.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : MonoBehaviour
+{
+    public Tilemap tilemap;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap.GetSprite(new Vector3Int(0, 0, 0));
+        for (int i = -5; i < 5; i++)
+        {
+            for (int j = -5; j < 5; j++)
+            {
+                Debug.Log(tilemap.GetSprite(new Vector3Int(i, j, 0)));
+            }
+        }
+    }
+}
---
+++ 基于网格化的a*/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject grid;
+    public float speed = 0.2f;
+    private List<Vector3> path = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetPositionAndRotation(grid.GetComponent<Grid>().FromVectorToPosition(new Vector2Int(0, 1)), Quaternion.identity);
+        transform.SetPositionAndRotation(new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+            path = grid.GetComponent<Grid>().FindWay(transform.position, mousePosition);
+        }
+        if (path.Count != 0)
+        {
+            if (Mathf.Abs(transform.position.x - path[path.Count - 1].x) > 0.001 || Mathf.Abs(transform.position.y - path[path.Count - 1].y) > 0.001)
+            {
+                Vector2 temp = Vector2.MoveTowards(transform.position, path[path.Count - 1], speed);
+                GetComponent<Rigidbody2D>().MovePosition(temp);
+            }
+            else
+            {
+                path.RemoveAt(path.Count - 1);
+            }
+
+        }
+    }
+}
